0da284c59e9d5eb2b506205c752d3df3beed0ef3
josinaldojr <josinaldo.pontes@dce.ufpb.br>
2019-08-10 18:04:49 -0300

    Criando operação de intanciação

--- src/main/java/com/naldosuri/cursomc/com/naldosuri/cursomc/CursomcApplication.java
+++ src/main/java/com/naldosuri/cursomc/com/naldosuri/cursomc/CursomcApplication.java
@@ -1,13 +1,34 @@
 package com.naldosuri.cursomc.com.naldosuri.cursomc;
 
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
-public class CursomcApplication {
+import com.naldosuri.cursomc.com.naldosuri.cursomc.domain.Categoria;
+import com.naldosuri.cursomc.com.naldosuri.cursomc.repositories.CategoriaRepository;
 
+@SpringBootApplication
+public class CursomcApplication implements CommandLineRunner {
+	
+	//Realizando a decalaração, e o uso do @AutoWired para instanciar automáticamente
+	@Autowired
+	private CategoriaRepository categoriaRepository;
+	
 	public static void main(String[] args) {
 		SpringApplication.run(CursomcApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		Categoria cat1 = new Categoria(null, "Informática");
+		Categoria cat2 = new Categoria(null, "Escritório");
+		
+		//Salvando as categorias do banco
+		categoriaRepository.saveAll(Arrays.asList(cat1,cat2));
+		
 	}
 
 }
