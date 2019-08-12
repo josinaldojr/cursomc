bba4ff9eb97e407bf20e3ab7f5c98ff860829af7
josinaldojr <josinaldo.pontes@dce.ufpb.br>
2019-08-12 15:20:30 -0300

    Produto e associação muitos para muitos

--- src/main/java/com/naldosuri/cursomc/com/naldosuri/cursomc/repositories/ProdutoRepository.java
+++ src/main/java/com/naldosuri/cursomc/com/naldosuri/cursomc/repositories/ProdutoRepository.java
@@ -3,10 +3,11 @@ package com.naldosuri.cursomc.com.naldosuri.cursomc.repositories;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import com.naldosuri.cursomc.com.naldosuri.cursomc.domain.Categoria;
+import com.naldosuri.cursomc.com.naldosuri.cursomc.domain.Produto;
 
 @Repository
-public interface CategoriaRepository extends JpaRepository<Categoria, Integer>{
+public interface ProdutoRepository extends JpaRepository<Produto, Integer>{
 	
+
 }
 
