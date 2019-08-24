da66aff464eb96a0e425a12c301b85b7cea43ae1
josinaldojr <josinaldo.pontes@dce.ufpb.br>
2019-08-24 01:39:24 -0300

    Paginação com parâmetros opcionais na requisição

--- src/main/java/com/naldosuri/cursomc/com/naldosuri/cursomc/services/CategoriaService.java
+++ src/main/java/com/naldosuri/cursomc/com/naldosuri/cursomc/services/CategoriaService.java
@@ -5,6 +5,9 @@ import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort.Direction;
 import org.springframework.stereotype.Service;
 
 import com.naldosuri.cursomc.com.naldosuri.cursomc.domain.Categoria;
@@ -46,5 +49,12 @@ public class CategoriaService {
 	
 	public List<Categoria> findAll(){
 		return repo.findAll();
+	}
+	
+	//O page vai encapsular informações e operações sobre a paginação
+	public Page<Categoria> findPage(Integer page, Integer linesPerPage, String orderBy, String direction){
+		//Objeto que prepara as informações para acessar qual a página que quer buscar//
+		PageRequest pageRequest = PageRequest.of(page, linesPerPage, Direction.valueOf(direction),orderBy);
+		return repo.findAll(pageRequest);
 	}
 }
